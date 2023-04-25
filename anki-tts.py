a66b2d972e02871ff80110b5a215c63a0c1ac033
VAJJHALA, SURYA (CORP) <surya.vajjhala@adp.com>
2023-04-25 13:46:35 -0400

    modified:   anki-tts.py 	new file:   requirements.txt

--- anki-tts.py
+++ anki-tts.py
@@ -1,63 +1,72 @@
 import os
 import shutil
 import tempfile
+import argparse
 from gtts import gTTS
 from anki.storage import Collection
 
-# Input path to the original .apkg file
-input_path = "test-data/french.apkg"
 
-# Output path for the updated .apkg file
-output_path = "test-data/output.apkg"
+def generate_audio_apkg(input_path, output_path, audio_path, text_index, speech_index):
+    '''
+    Fn to perform TextToSPeech on a given file of an ANKI package and add it to another field
+    '''
 
-# Path to the folder where audio files will be saved
-audio_path = "audio"
+    # Create a temporary directory to extract the contents of the original .apkg file
+    with tempfile.TemporaryDirectory() as temp_dir:
 
+        # Extract the contents of the original .apkg file to the temporary directory
+        shutil.unpack_archive(input_path, temp_dir, "zip")
 
-# Create a temporary directory to extract the contents of the original .apkg file
-with tempfile.TemporaryDirectory() as temp_dir:
+        # Load the collection.anki2 file from the extracted directory
+        collection_path = os.path.join(temp_dir, "collection.anki2")
+        collection = Collection(collection_path)
 
-    # Extract the contents of the original .apkg file to the temporary directory
-    shutil.unpack_archive(input_path, temp_dir, "zip")
+        # Modify the notes in the collection
+        for note_id in collection.find_notes(""):
+            print(note_id)
+            note = collection.get_note(note_id)
 
-    # Load the collection.anki2 file from the extracted directory
-    collection_path = os.path.join(temp_dir, "collection.anki2")
-    collection = Collection(collection_path)
+            # Get the French text from the note
+            french_text = note.fields[text_index]
 
-    # Modify the notes in the collection
-    for note_id in collection.find_notes(""):
-        note = collection.get_note(note_id)
+            # Generate the audio file
+            tts = gTTS(french_text, lang="fr")
+            audio_file = "anki-tts-{}.mp3".format(note_id)
+            audio_file_path = os.path.join(audio_path, audio_file)
+            tts.save(audio_file_path)
 
-        # Get the French text from the note
-        french_text = note.fields[1]
+            # Add the audio file to the note
+            note.fields[speech_index] = "[sound:{}]".format(audio_file)
 
-        # Generate the audio file
-        tts = gTTS(french_text, lang="fr")
-        audio_file = f"{note_id}.mp3"
-        audio_file_path = os.path.join(audio_path, audio_file)
-        tts.save(audio_file_path)
+            # Update the note in the collection
+            collection.update_note(note)
 
-        # Add the audio file to the note
-        note.fields[3] = f"[sound:{audio_file}]"
+        # Save the changes to the collection
+        collection.close()
 
-        # Update the note in the collection
-        collection.update_note(note)
+        # Create a new .apkg file in a separate directory
+        with tempfile.TemporaryDirectory() as output_dir:
 
-    # Save the changes to the collection
-    collection.close()
+            # Compress the modified contents into a new .apkg file
+            shutil.make_archive(
+                os.path.join(output_dir, "updated"),
+                "zip",
+                temp_dir
+            )
 
-    # Create a new .apkg file in a separate directory
-    with tempfile.TemporaryDirectory() as output_dir:
+            # Copy the new .apkg file to the output directory
+            shutil.copy(
+                os.path.join(output_dir, "updated.zip"),
+                output_path
+            )
 
-        # Compress the modified contents into a new .apkg file
-        shutil.make_archive(
-            os.path.join(output_dir, "updated"),
-            "zip",
-            temp_dir
-        )
-
-        # Copy the new .apkg file to the output directory
-        shutil.copy(
-            os.path.join(output_dir, "updated.zip"),
-            output_path
-        )
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate audio files and create updated .apkg file')
+    parser.add_argument('input_path', type=str, help='Path to the original .apkg file')
+    parser.add_argument('output_path', type=str, help='Path to the output .apkg file')
+    parser.add_argument('audio_path', type=str, help='Path to the folder where audio files will be saved')
+    parser.add_argument('text_index', type=int, help='index of the text field in a given note. Make sure text has no HTML formatting')
+    parser.add_argument('speech_index', type=int, help='index of the speech feld in a given note')
+    args = parser.parse_args()
+    
+    generate_audio_apkg(args.input_path, args.output_path, args.audio_path, args.text_index, args.speech_index)
--- venvx/bin/activate.fish
+++ venvx/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/vajjhals/repos.personal/anki-recordings/venvx"
+set -gx VIRTUAL_ENV "/Users/vajjhals/repos.personal/anki-tts/venvx"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
